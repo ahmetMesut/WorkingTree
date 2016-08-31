8df3a3443e6cbec06f9e667ffa1ab4703deeacad
Ahmet Mesut Şahinoğlu <ahmet.sahinoglu@mebitech.com>
2016-08-31 11:43:25 +0300

    first

--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 public class NumberTest {
 
+
     @Test
     public void findHighestTest() {
         assertEquals(87, Number.findHighestNumber(new int[]{52, 87, 68, 72}));
